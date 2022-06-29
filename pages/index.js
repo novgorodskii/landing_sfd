b21eb90cc69d05f78b9af970e8947d4505d19310
Novgorodskii Igor <novgorodskiji@gmail.com>
2022-06-29 22:19:08 +0300

    update btn

--- pages/index.js
+++ pages/index.js
@@ -40,10 +40,10 @@ export default function Home() {
                 <a className="link-menu font-size-xs font-weight-md">Blog</a>
               </li>
             </ul>
-            <a onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md" href="#">
+            <div onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md">
               Launch App
               <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-            </a>
+            </div>
           </div>
           <div className="wrapper-burger-menu">
             <input type="checkbox" id="check-menu" />
@@ -94,10 +94,10 @@ export default function Home() {
           >
             Decentralized Contracts for Differences protocol
           </h1>
-          <a onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md" href="#">
+          <div onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md">
             Start Trading
             <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-          </a>
+          </div>
         </div>
       </div>
     </header>
@@ -280,10 +280,10 @@ export default function Home() {
                 date, the exercise price and the maximum deviation from the
                 current price covered by the contract.
               </p>
-              <a onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md" href="#">
+              <div onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md">
                 Start Trading
                 <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-              </a>
+              </div>
             </div>
             <ul className="how-it-works-list">
               <li className="how-it-works-list-item offset-xs">
@@ -392,10 +392,10 @@ export default function Home() {
             >
               Trade as a professional!
             </h5>
-            <a onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md" href="#">
+            <div onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md">
               Launch App
               <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-            </a>
+            </div>
           </div>
         </div>
       </div>
@@ -478,10 +478,10 @@ export default function Home() {
                 src="/images/logo-dark.svg"
                 alt="image-logo-footer"
               />
-              <a onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md" href="#">
+              <div onClick={() => setShow(true)} className="wrapper-link-button text-md font-weight-md">
                 Launch App
                 <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-              </a>
+              </div>
             </div>
           </div>
           <div className="col-md">
@@ -597,10 +597,10 @@ export default function Home() {
               </ul>
             </div>
           </div>
-          <a className="wrapper-link-button text-md font-weight-md" onClick={() => setShow(true)} href="#"
+          <div className="wrapper-link-button text-md font-weight-md" onClick={() => setShow(true)}
             >Launch App
             <svg className="wrapper-arrow-img" width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.75 5.25a.75.75 0 0 0 0 1.5H9l-3.593 3.592a.753.753 0 0 0 1.066 1.065l4.7-4.7a1 1 0 0 0 0-1.415l-4.7-4.7a.753.753 0 1 0-1.066 1.065L9 5.25H.75Z"/></svg>
-          </a>
+          </div>
         </div>
       </div>
     </section>
