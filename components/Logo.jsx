d7a1773941a7f036a5620716e28b5f1f2162d7d8
vlad-shulzhenko <shulzhenkovlad01@gmail.com>
2023-03-13 18:15:30 +0200

    fixed logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,13 +1,24 @@
+"use client";
+import { useState, useEffect } from "react";
 import { useMediaQuery } from "@chakra-ui/react";
 import Image from "next/image"
 import Link from "next/link";
 
 const Logo = () => {
+  const [logoSrc, setLogoSrc] = useState('/images/logo.svg');
   const [isMobile] = useMediaQuery('(max-width: 619px)');
+  useEffect(() => {
+    if (isMobile) {
+      setLogoSrc('/images/simple-logo.svg')
+    } else {
+      setLogoSrc('/images/logo.svg')
+    }
+  }, [isMobile])
+
   return (
     <Link href="/" style={{ cursor: 'pointer' }}>
       <Image
-        src={isMobile ? '/images/simple-logo.svg' : '/images/logo.svg'}
+        src={logoSrc}
         alt="logo"
         width={isMobile ? 43 : 213}
         height={isMobile ? 50 : 60}
